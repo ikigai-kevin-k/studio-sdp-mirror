70e42c5c3dee304c9d86fe5f924884444eee5f9c
ikigai-kevin-k <kevin.k@ikigai.team>
2024-11-07 11:41:51 +0800

    non-arcade mode: 1. Added `check_receive_force_restart_game` to the roulette_main_thread. 2. Split the force restart command from the state_discriminator to a separate function,called `check_receive_force_restart_game`. 3. README

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ The SDP CLI interface:
 ![SDP CLI](img/sdp-interface-cli.png)
 The terminal log with color:
 ![terminal log with color](img/correct_log.png)
+
+New features:
+1. Added `check_receive_force_restart_game` to the roulette_main_thread.
+2. Split the force restart command from the state_discriminator to a separate function,
+    called `check_receive_force_restart_game`.
+Log example of force restart:
+![alt text](sim/img/force-restart.png)
--- sim/core/roulette_sim.py
+++ sim/core/roulette_sim.py
@@ -76,10 +76,13 @@ class RouletteSimulator(StateMachine):
 
     def __init__(self):
         super().__init__()
+        self.line_number = 1
         self.masterRoulettePort, self.slaveRoulettePort = self.create_virtual_serial_port()
         self.thread = threading.Thread(target=self.roulette_main_thread, args=(self.masterRoulettePort,))
         self.thread.daemon = True
         self.thread.start()
+
+        # self.line_number = 1
 
     def create_virtual_serial_port(self):
         master, slave = pty.openpty()
@@ -431,7 +434,7 @@ class RouletteSimulator(StateMachine):
             log_with_color(data)
             raise Exception("unknown protocol log type.")
 
-    def read_ss2_protocol_log(self, file_name, line_number):
+    def read_ss2_protocol_log(self, file_name):# line_number):
         """
         This is a utility function to read the protocol log file.
         Should be moved to a utility module.
@@ -439,7 +442,7 @@ class RouletteSimulator(StateMachine):
         try:
             with open(file_name, "r") as file:
                
-                for _ in range(line_number - 1):
+                for _ in range(self.line_number - 1):
                     next(file)
                 
                 line = next(file, None)
@@ -471,13 +474,13 @@ class RouletteSimulator(StateMachine):
             log_with_color(f"Roulette supposed to be received from SDP: {read_data.decode().strip()}")
 
     def roulette_main_thread(self, master):
-        line_number = 1
+        self.line_number = 1
         while True:
             try:
-                log_with_color(f"{GREEN}Line number: {line_number}{RESET}")
+                log_with_color(f"{GREEN}Line number: {self.line_number}{RESET}")
                 print("\n")
                 self.roulette_state_display()
-                data = self.read_ss2_protocol_log(LOG_FILE_NAME,line_number)
+                data = self.read_ss2_protocol_log(LOG_FILE_NAME)#,self.line_number)
   
                 if not data:
                     log_with_color("Reached end of log file. Terminating program...")
@@ -487,22 +490,299 @@ class RouletteSimulator(StateMachine):
                     self.roulette_write_data_to_sdp(data)
                     self.roulette_read_data_from_sdp()
                 except Exception as e:
-                    log_with_color(f"Error processing line {line_number}: {e}")
+                    log_with_color(f"Error processing line {self.line_number}: {e}")
                     break
                 
-                line_number += 1
+                self.line_number += 1
                 time.sleep(self.LOG_FREQUENCY)
                 
             except OSError as e:
                 log_with_color(f"Serial port error: {e}")
                 break
 
+class TestRouletteSimulatorNonArcade(RouletteSimulator):
+
+
+    def __init__(self):
+        self.line_number = 1
+        super().__init__()
+
+    def check_receive_force_restart_game(self,data):
+        """
+        Read from the log line containing "*R"
+        """
+        if "*R" in data:
+            self.reset_state_transition_waiting_counter()
+            self.current_game_state = "start_game"
+            self.current_data_protocol_mode = "game_mode"
+            self.current_power_state = "on"
+            """
+            Simulate the *R\r\n command 
+            While encountering the WF, append the *R\r\n command to the log file
+            """
+            print(f"{YELLOW}*R{RESET}")
+            print(f"{YELLOW}*R ok{RESET}")
+            self.line_number = 3 # from game start state
+            return True
+        else:
+            return False
+    def roulette_main_thread(self, master):
+        """
+        Split the force restart command from the state_discriminator to a separate function,
+        called `check_receive_force_restart_game`.
+        Added `check_receive_force_restart_game` to the roulette_main_thread.
+        """
+        while True:
+            try:
+                log_with_color(f"{GREEN}Line number: {self.line_number}{RESET}")
+                print("\n")
+                self.roulette_state_display()
+                data = self.read_ss2_protocol_log(LOG_FILE_NAME)#,self.line_number)
+  
+                if not data:
+                    log_with_color("Reached end of log file. Terminating program...")
+                    break
+                try:
+                    if self.check_receive_force_restart_game(data):
+                        continue
+                    self.state_discriminator(data)
+                    self.roulette_write_data_to_sdp(data)
+                    self.roulette_read_data_from_sdp()
+                except Exception as e:
+                    log_with_color(f"Error processing line {self.line_number}: {e}")
+                    break
+                
+                self.line_number += 1
+                time.sleep(self.LOG_FREQUENCY)
+                
+            except OSError as e:
+                log_with_color(f"Serial port error: {e}")
+                break
+
+     
+    # def state_discriminator(self):
+
+    #     """
+    #     """
+    #     data = protocol_log_line
+
+    #     print("\n")
+    #     print("log line:",data)
+    #     print("current_data_protocol_mode:",self.current_data_protocol_mode)
+    #     print("current_game_state:",self.current_game_state)
+    #     print("current_power_state:",self.current_power_state)
+
+    #     if "*X;" in data:
+    #         print(f"{GREEN}X1 state transition waiting counter:{RESET}",self.x1_state_transition_waiting_counter)
+    #         print(f"{GREEN}X2 state transition waiting counter:{RESET}",self.x2_state_transition_waiting_counter)
+    #         print(f"{GREEN}X3 state transition waiting counter:{RESET}",self.x3_state_transition_waiting_counter)
+    #         print(f"{GREEN}X4 state transition waiting counter:{RESET}",self.x4_state_transition_waiting_counter)
+    #         print(f"{GREEN}X5 state transition waiting counter:{RESET}",self.x5_state_transition_waiting_counter)
+    #         print(f"{GREEN}X6 state transition waiting counter:{RESET}",self.x6_state_transition_waiting_counter)
+    #         print(f"{GREEN}P0 delay counter:{RESET}",self.p0_delay_counter)
+            
+    #         if "*X;1" in data:
+    #             try:
+    #                 if self.current_game_state == "table_closed":
+    #                     self.current_data_protocol_mode = "game_mode"
+    #                     self.current_game_state = "start_game"
+    #                     self.reset_state_transition_waiting_counter()
+    #                     return
+    #                 elif self.current_game_state == "start_game":
+    #                     if self.x1_state_transition_waiting_counter < self.X1_MAX_WAITING_TIME:
+    #                         self.x1_state_transition_waiting_counter += 1
+    #                         return
+    #                     else:
+    #                         raise Exception(f"{RED}state transition time too long, there may be something wrong.{RESET}")
+    #             except Exception as e:
+    #                 log_with_color(f"Error asserting state transition: {e}")
+    #                 raise Exception("state transition error, close the program.")
+            
+    #         elif "*X;2" in data:
+    #             try:
+    #                 if self.current_game_state == "start_game":
+    #                     self.current_game_state = "place_bet"
+    #                     self.reset_state_transition_waiting_counter()
+    #                     return
+                    
+    #                 elif self.current_game_state == "winning_number":
+    #                     self.current_game_state = "place_bet"
+    #                     self.reset_state_transition_waiting_counter()
+    #                     return
+                    
+    #                 elif self.current_game_state == "place_bet":
+    #                     if self.x2_state_transition_waiting_counter < self.X2_MAX_WAITING_TIME:
+    #                         self.x2_state_transition_waiting_counter += 1
+    #                         return
+    #                     else:
+    #                         raise Exception(f"{RED}state transition time too long, there may be something wrong.{RESET}")
+    #             except Exception as e:
+    #                 log_with_color(f"Error asserting state transition: {e}")
+    #                 raise Exception("state transition error, close the program.")
+
+    #         elif "*X;3" in data:
+    #             try:
+    #                 if self.current_game_state == "place_bet":
+    #                     self.current_game_state = "ball_launch"
+    #                     self.reset_state_transition_waiting_counter()
+    #                     return
+    #                 elif self.current_game_state == "ball_launch":
+    #                     if self.x3_state_transition_waiting_counter < self.X3_MAX_WAITING_TIME:
+    #                         self.x3_state_transition_waiting_counter += 1
+    #                         return
+    #                     else:
+    #                         # self.force_restart_game()
+    #                         raise Exception(f"{RED}state transition time too long, there may be something wrong.{RESET}")
+    #             except Exception as e:
+    #                 log_with_color(f"Error asserting state transition: {e}")
+    #                 raise Exception("state transition error, close the program.")
+    #         elif "*X;4" in data:
+    #             try:
+    #                 # assert self.current_game_state == "ball_launch" or \
+    #                 #        self.current_game_state == "no_more_bet"
+                    
+    #                 if self.current_game_state == "ball_launch":
+    #                     self.current_game_state = "no_more_bet"
+    #                     self.reset_state_transition_waiting_counter()
+    #                     return
+    #                 elif self.current_game_state == "no_more_bet":
+    #                     if self.x4_state_transition_waiting_counter < self.X4_MAX_WAITING_TIME:
+    #                         self.x4_state_transition_waiting_counter += 1
+    #                         return
+    #                     else:
+    #                         # self.force_restart_game()
+    #                         raise Exception(f"{RED}state transition time too long, there may be something wrong.{RESET}")
+    #             except Exception as e:
+    #                 log_with_color(f"Error asserting state transition: {e}")
+    #                 raise Exception("state transition error, close the program.")
+    #         elif "*X;5" in data or data.strip().isdigit():
+    #             try:
+    #                 # assert self.current_game_state == "no_more_bet" or\
+    #                 #        self.current_game_state == "winning_number"
+    #                 if self.current_game_state == "no_more_bet":
+    #                     self.current_game_state = "winning_number"
+    #                     self.reset_state_transition_waiting_counter()
+    #                     return
+    #                 elif self.current_game_state == "winning_number":
+    #                     if self.x5_state_transition_waiting_counter < self.X5_MAX_WAITING_TIME:
+    #                         self.x5_state_transition_waiting_counter += 1
+    #                         return
+    #                     else:
+    #                         # self.force_restart_game()
+    #                         raise Exception(f"{RED}state transition time too long, there may be something wrong.{RESET}")
+    #             except Exception as e:
+    #                 log_with_color(f"Error asserting state transition: {e}")
+    #                 raise Exception("state transition error, close the program.")
+
+    #         elif "*X;6" in data:
+    #             try:
+    #                 if self.current_game_state != "table_closed":
+    #                     self.current_game_state = "table_closed"
+    #                     self.reset_state_transition_waiting_counter()
+    #                     return
+    #                 else:
+    #                     if self.x6_state_transition_waiting_counter < self.X6_MAX_WAITING_TIME:
+    #                         self.x6_state_transition_waiting_counter += 1
+    #                         return
+    #                     else:
+    #                         # self.force_close_table()
+    #                         raise Exception(f"{RED}state transition time too long, there may be something wrong.{RESET}")
+    #             except Exception as e:
+    #                 log_with_color(f"Error asserting state transition: {e}")
+    #                 raise Exception("state transition error, close the program.")
+
+    #         else:
+    #             log_with_color(f"unknown game state: {data}")
+    #             raise Exception("unknown game state, close the program.")
+
+    #     elif "*R" in data:
+    #         self.force_restart_game()
+
+    #     elif "*o" in data:
+    #         self.current_data_protocol_mode = "operation_mode"
+    #         """
+    #         Operation mode
+    #         """
+    #         pass
+    #     elif "*F" in data:
+    #         self.current_data_protocol_mode = "self_test_mode"
+    #         """
+    #         Self-test mode
+    #         """
+    #         pass
+    #     elif "*P" in data:
+    #         """
+    #         Power setting mode
+    #         """
+            
+    #         if "*P 1" in data and self.current_power_state == "off":
+    #             """add a condition: the next line is *P OK"""
+    #             self.current_power_state = "on"
+    #             """In arcade mode, power on will trigger table open"""
+    #             self.current_data_protocol_mode = "power_setting_mode"
+    #             return
+            
+    #         elif "*P 0" in data and self.current_power_state == "on":
+    #             """add a condition: the next line is *P OK"""
+    #             self.current_power_state = "off"
+    #             """off will trigger table force close"""
+    #             self.current_game_state = "table_closed"
+    #             self.current_data_protocol_mode = "power_setting_mode"
+
+    #             if "*X:6" not in data and self.p0_delay_counter < self.P0_MAX_DELAY:
+    #                 self.p0_delay_counter += 1
+    #             else:
+    #                 raise Exception(f"{RED}P0 delay time too long, there may be something wrong.{RESET}")
+    #             return
+            
+    #         elif "*P OK" in data:
+    #             pass
+    #             return
+    #         else:
+    #             log_with_color(data)
+    #             raise Exception("unknown power state.")            
+
+    #     elif "*C" in data:
+    #         self.current_data_protocol_mode = "calibration_mode"
+    #         """
+    #         Calibration mode
+    #         """
+    #         pass
+    #     elif "*W" in data:
+    #         self.current_data_protocol_mode = "warning_flag_mode"
+    #         """
+    #         Warning flag mode
+    #         Restart the game
+    #         """
+    #         # self.game_state_transition_to("start_game")
+    #         self.current_game_state = "start_game"
+    #         pass
+    #     elif "*M" in data:
+    #         self.current_data_protocol_mode = "statistics_mode"
+    #         """
+    #         Statistics mode
+    #         """
+    #         pass
+
+    #     elif data.strip().isdigit():                 
+    #         """
+    #         The number of the winning number
+    #         """
+    #         pass
+    #     else:
+    #         log_with_color(data)
+    #         raise Exception("unknown protocol log type.")
+
+
+    
+
 if __name__ == "__main__":
 
     global LOG_FILE_NAME
     LOG_FILE_NAME = "../log/ss2/ss2_protocol2.log"
     try:
-        roulette = RouletteSimulator()
+        roulette = TestRouletteSimulatorNonArcade()
+        # roulette = RouletteSimulator()
         log_with_color(f"Roulette simulator is running. Virtual port: {roulette.slaveRoulettePort}")
         log_with_color("Press Ctrl+C to stop the simulator.")
 
