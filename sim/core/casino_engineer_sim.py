866aff110f5135ee0f28bf2e52122965120df1c2
ikigai-kevin-k <kevin.k@ikigai.team>
2025-03-07 10:22:55 +0800

    rm inrelative folder, file for refactor

--- sim/core/casino_engineer_sim.py
+++
@@ -1,7 +0,0 @@
-"""
-Simulate the casino engineer's operation of sending command directly to the SDP interface toward the roulette.
-"""
-
-class CasinoEngineerSimulator:
-    def __init__(self):
-        pass
